ee9b8cf6c63a6c47675bc9b5625403215e59bfa3
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-03-01 05:27:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function BoardMember(name, homeState, training) {
   this.training = training;
 }
 
+<<<<<<< HEAD
 BoardMember.prototype.veto = function() {
   return 'No, I must disagree';
 }
@@ -18,4 +19,20 @@ BoardMember.prototype.releasePressStatement = function() {
 }
 BoardMember.prototype.sayHi = function() {
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+=======
+BoardMember.prototype.veto() {
+  return 'No, I must disagree';
+}
+BoardMember.prototype.approve() {
+  return 'You can do that!';
+}
+BoardMember.prototype.doCharity() {
+  return 'I like to help people.';
+}
+BoardMember.prototype.releasePressStatement() {
+  return 'You will see great things from Scuber.';
+}
+BoardMember.prototype.sayHi() {
+  return `Hi, mu name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`;
+>>>>>>> 5d9851adfdcf3acc0f96e9ba17fab61c0baa5150
 }
